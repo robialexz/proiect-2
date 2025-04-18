72fbe04284faf0cd15dd2321ad9602a5791aebbd
robialexz <robialexz@gmail.com>
2025-04-18 20:08:16 +0300

    Modernizare UI/UX: design system, login, feedback vizual, AI roadmap, structura modulară

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,12 +3,14 @@ export * from "./utils";
 export * from "./performance";
 export * from "./image-optimization";
 export * from "./form-optimization";
+export * from "./lazy-pages";
 
 // Export default objects
 import utils from "./utils";
 import performance from "./performance";
 import imageOptimization from "./image-optimization";
 import formOptimization from "./form-optimization";
+import lazyPages from "./lazy-pages";
 
 // Combine all utilities into a single object
 const lib = {
@@ -16,6 +18,7 @@ const lib = {
   ...performance,
   ...imageOptimization,
   ...formOptimization,
+  ...lazyPages,
 };
 
 export default lib;
